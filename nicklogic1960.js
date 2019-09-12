efd1797126c91e83641eaa86b7add014eb6b3e85
Nick Benjamin <NBLaptop@Nicks-MacBook-Air.local>
2019-09-11 21:17:36 -0400

    1960 map in js

--- nicklogic1960.js
+++ nicklogic1960.js
@@ -21,126 +21,126 @@ var cities = [
 	  {
     name: "Anchorage",
     location: [61.17755, -149.274],
-    population: 11254,
+    population: 44237,
     snow:"true"
   },
 
 {
     name: "Phoenix",
     location: [33.57216, -112.088],
-    population: 106818,
+    population: 439170,
     snow:"false"
   },
 
 {
     name: "Los Angeles",
     location: [34.01939, -118.411],
-    population: 1970358,
+    population: 2479015,
     snow:"false"
   },
 
 {
     name: "San Diego",
     location: [32.8153, -117.135],
-    population: 334387,
+    population: 2479015,
     snow:"false"
   },
 
 {
     name: "Miami",
     location: [25.77516, -80.2086],
-    population: 249276,
+    population: 291688,
     snow:"false"
   },
 
 {
     name: "Atlanta",
     location: [33.76291, -84.4227],
-    population: 331314,
+    population: 487455,
     snow:"false"
   },
 
 {
     name: "Honolulu",
     location: [21.32585, -157.845],
-    population: 248034,
+    population: 294194,
     snow:"false"
   },
 
 {
     name: "Chicago",
     location: [41.83755, -87.6818],
-    population: 3620962,
+    population: 3550404,
     snow:"true"
   },
 
 {
     name: "Fort Wayne",
     location: [41.08817, -85.1439],
-    population: 133607,
+    population: 161776,
     snow:"true"
   },
 
 {
     name: "Louisville",
     location: [38.17808, -85.6667],
-    population: 369129,
+    population: 390639,
     snow:"true"
   },
 
 {
     name: "Detroit",
     location: [42.38304, -83.1022],
-    population: 1849568,
+    population: 1670144,
     snow:"true"
   },
 
 {
     name: "Minneapolis",
     location: [44.96332, -93.2683],
-    population: 521718,
+    population: 482872,
     snow:"true"
   },
 
 {
     name: "New York City",
     location: [40.66427, -73.9385],
-    population: 7891957,
+    population: 7781984,
     snow:"true"
   },
 
 {
     name: "Cleveland",
     location: [41.47814, -81.6795],
-    population: 914808,
+    population: 876050,
     snow:"true"
   },
 
 {
     name: "Nashville",
     location: [36.1718, -86.785],
-    population: 174307,
+    population: 170874,
     snow:"false"
   },
 
 {
     name: "Corpus Christi",
     location: [27.80455, -97.3963],
-    population: 108287,
+    population: 167690,
     snow:"false"
   },
 
 {
     name: "Houston",
     location: [29.78047, -95.3863],
-    population: 596163,
+    population: 938219,
     snow:"false"
   },
 
 {
     name: "Las Vegas",
     location: [36.0719, -115.1634],
-    population: 24624,
+    population: 64,405,
     snow:"false"
   },
 
@@ -148,15 +148,24 @@ var cities = [
 
 // Loop through the cities array and create one marker for each city object
 for (var i = 0; i < cities.length; i++) {
-	if (cities[i].snow == "false")
-    	{fllcolor="yellow";}
-    else {fllcolor="blue";}
-  L.circle(cities[i].location, {
-    fillOpacity: 0.75,
-    color: "white",
-    fillColor: fllcolor,
+
+  var sunicon = L.icon({
+    iconUrl:'Sun_icon.png',
     // Setting our circle's radius equal to the output of our markerSize function
     // This will make our marker's size proportionate to its population
-    radius: markerSize(cities[i].population)
-  }).bindPopup("<h1>" + cities[i].name + "</h1> <hr> <h3>Population: " + cities[i].population + "</h3>").addTo(myMap);
+    iconSize:[(cities[i].population)/40000,(cities[i].population)/40000],
+  }
+  );
+  var snowicon = L.icon({
+    iconUrl:'HieloSnowflake.png',
+    // Setting our circle's radius equal to the output of our markerSize function
+    // This will make our marker's size proportionate to its population
+    iconSize:[(cities[i].population)/40000,(cities[i].population)/40000],
+  }
+  );
+	if (cities[i].snow == "false")
+    	{icon=sunicon;}
+    else {icon=snowicon;}
+  L.marker(cities[i].location, {icon: icon}).bindPopup("<h1>" + cities[i].name + "</h1> <hr> <h3>Population: " + cities[i].population + "</h3>").addTo(myMap);
+
 }
