594a6316bfbdbc92d6ed222718b5a4ad2319b40a
Nick Benjamin <NBLaptop@Nicks-MacBook-Air.local>
2019-09-15 20:55:39 -0400

    ending SQL for heroku

--- App/city_pop/app.py
+++ App/city_pop/app.py
@@ -8,16 +8,16 @@ from sqlalchemy import create_engine, func
 # Database Setup
 #################################################
 #engine = create_engine("sqlite:///titanic.sqlite")
-engine = create_engine("sqlite:///citypop.sqlite")
+#engine = create_engine("sqlite:///citypop.sqlite")
 
 # reflect an existing database into a new model
-Base = automap_base()
+#Base = automap_base()
 # reflect the tables
-Base.prepare(engine, reflect=True)
+#Base.prepare(engine, reflect=True)
 
 # Save reference to the table
 #Passenger = Base.classes.passenger
-onlytable2=Base.classes.onlytable2
+#onlytable2=Base.classes.onlytable2
 
 # Create our session (link) from Python to the DB
 
@@ -70,14 +70,17 @@ def twothousandten():
 
 @app.route("/citypop")
 def citypop():
-    """Return a list of total population of sunny vs. not sunny over time"""
-    session = Session(engine)
-    results = session.query(onlytable2.date,onlytable2.SNOW,onlytable2.NOSNOW).all()
+   return render_template("citypop.html") 
 
-    # Convert list of tuples into normal list
-    citypops = list(np.ravel(results))
-
-    return jsonify(citypops)
+#def citypop():
+#    """Return a list of total population of sunny vs. not sunny over time"""
+#    session = Session(engine)
+#    results = session.query(onlytable2.date,onlytable2.SNOW,onlytable2.NOSNOW).all()
+#
+#    # Convert list of tuples into normal list
+#    citypops = list(np.ravel(results))
+#
+#    return jsonify(citypops)
 
 
  
