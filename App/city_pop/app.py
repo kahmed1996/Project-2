a3c356bd1f14a5242362cf98381041511453a443
Nick Benjamin <NBLaptop@Nicks-MacBook-Air.local>
2019-09-16 18:17:06 -0400

    for presentation

--- App/city_pop/app.py
+++ App/city_pop/app.py
@@ -1,25 +1,4 @@
-from flask import Flask, render_template, current_app, url_for, jsonify
-import numpy as np
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-#################################################
-# Database Setup
-#################################################
-#engine = create_engine("sqlite:///titanic.sqlite")
-#engine = create_engine("sqlite:///citypop.sqlite")
-
-# reflect an existing database into a new model
-#Base = automap_base()
-# reflect the tables
-#Base.prepare(engine, reflect=True)
-
-# Save reference to the table
-#Passenger = Base.classes.passenger
-#onlytable2=Base.classes.onlytable2
-
-# Create our session (link) from Python to the DB
+from flask import Flask, render_template, current_app, url_for
 
 
 app = Flask(__name__)
@@ -72,15 +51,6 @@ def twothousandten():
 def citypop():
    return render_template("citypop.html") 
 
-#def citypop():
-#    """Return a list of total population of sunny vs. not sunny over time"""
-#    session = Session(engine)
-#    results = session.query(onlytable2.date,onlytable2.SNOW,onlytable2.NOSNOW).all()
-#
-#    # Convert list of tuples into normal list
-#    citypops = list(np.ravel(results))
-#
-#    return jsonify(citypops)
 
 
  
