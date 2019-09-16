b4c6bc7a49a1c0da5fa7daa7166b634925cf58ca
Nick Benjamin <NBLaptop@Nicks-MacBook-Air.local>
2019-09-15 20:09:18 -0400

    Flask app in new subfolder

---
+++ PopulationSnowVsSunGraph/Flask JSONifies/app.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+#engine = create_engine("sqlite:///titanic.sqlite")
+engine = create_engine("sqlite:///citypop.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+#Passenger = Base.classes.passenger
+onlytable2=Base.classes.onlytable2
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+#        f"/api/v1.0/names<br/>"
+#        f"/api/v1.0/passengers<br>"
+        f"/citypop"
+    )
+
+
+@app.route("/api/v1.0/names")
+#def names():
+#    """Return a list of all passenger names"""
+#    # Query all passengers
+#    session = Session(engine)
+#    results = session.query(Passenger.name).all()
+#
+#    # Convert list of tuples into normal list
+#    all_names = list(np.ravel(results))
+#
+#    return jsonify(all_names)
+#
+#
+#@app.route("/api/v1.0/passengers")
+#def passengers():
+#    """Return a list of passenger data including the name, age, and sex of each passenger"""
+#    # Query all passengers
+#    session = Session(engine)
+#    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+#
+#    # Create a dictionary from the row data and append to a list of all_passengers
+#    all_passengers = []
+#    for name, age, sex in results:
+#        passenger_dict = {}
+#        passenger_dict["name"] = name
+#        passenger_dict["age"] = age
+#        passenger_dict["sex"] = sex
+#        all_passengers.append(passenger_dict)
+#
+#    return jsonify(all_passengers)
+
+@app.route("/citypop")
+def citypop():
+    """Return a list of total population of sunny vs. not sunny over time"""
+    session = Session(engine)
+    results = session.query(onlytable2.date,onlytable2.SNOW,onlytable2.NOSNOW).all()
+
+    # Convert list of tuples into normal list
+    citypops = list(np.ravel(results))
+
+    return jsonify(citypops)
+
+if __name__ == '__main__':
+    app.run(debug=True)
