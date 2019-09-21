f71b784ec4a0ce68700ae9df4a7a6e32f2fd647b
Nick Benjamin <NBLaptop@Nicks-MacBook-Air.local>
2019-09-21 14:52:03 -0400

    final push

--- App/bootcampproject2v2/app.py
+++ App/bootcampproject2v2/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, current_app, url_for
+from flask import Flask, render_template, current_app, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import psycopg2
 import sys
 import json
 from flask_heroku import Heroku
@@ -7,8 +8,11 @@ import numpy as np
 
 app = Flask( __name__ )
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 heroku = Heroku(app)
 db = SQLAlchemy(app)
+# db.create_all()
+
 
 @app.route("/")
 def home():
@@ -59,13 +63,59 @@ def citypop():
    return render_template("citypop.html") 
 
 class Dataentry(db.Model):
-    __tablename__ = "dataentry"
-    id = db.Column(db.Integer, primary_key=True)
-    mydata = db.Column(db.Text())
+	__tablename__ = "data"
+	date = db.Column(db.String, primary_key=True)
+	anchorage = db.Column(db.Integer)
+	chicago = db.Column(db.Integer)
+	fort_wayne = db.Column(db.Integer)
+	louisville = db.Column(db.Integer)
+	detroit = db.Column(db.Integer)	
+	minneapolis = db.Column(db.Integer)
+	new_york_city = db.Column(db.Integer)
+	cleveland = db.Column(db.Integer)
+	snow = db.Column(db.Integer)
+	phoenix = db.Column(db.Integer)
+	los_angeles = db.Column(db.Integer)	
+	san_diego = db.Column(db.Integer)
+	miami = db.Column(db.Integer)
+	atlanta = db.Column(db.Integer)
+	honolulu = db.Column(db.Integer)
+	nashville = db.Column(db.Integer)
+	corpus_christi = db.Column(db.Integer)
+	houston = db.Column(db.Integer)
+	las_vegas = db.Column(db.Integer)
+	nosnow  = db.Column(db.Integer)
+	
+	def __init__ (self, mydata):
+		self.date=date
+		self.anchorage=anchorage
+		self.chicago=chicago
+		self.fort_wayne=chicago
+		self.louisville=louisville
+		self.detroit=detroit,
+		self.minneapolis=minneapolis
+		self.new_york_city=new_york_city
+		self.cleveland=cleveland
+		self.snow=snow
+		self.phoenix=phoenix
+		self.los_angeles=los_angeles
+		self.san_diego=los_angeles
+		self.miami=miami
+		self.atlanta=atlanta
+		self.honolulu=honolulu
+		self.nashville =nashville
+		self.corpus_christi=corpus_christi
+		self.houston=houston
+		self.las_vegas=las_vegas
+		self.nosnow=nosnow
 
-    def __init__ (self, mydata):
-        self.mydata = mydata
-
+   
+@app.route("/dbreturn")
+def dbreturn():
+	results = db.session.query(Dataentry.date,Dataentry.snow,Dataentry.nosnow).all()
+	# Convert list of tuples into normal list
+	citypopsz = list(np.ravel(results))
+	return jsonify(citypopsz)
 
 @app.route("/submit", methods=["POST"])
 def post_to_db():
